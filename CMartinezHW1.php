a269dfc96236cf3f3ede5d19fca3953adb5fc723
MrRocknRolla <31430645+MrRocknRolla@users.noreply.github.com>
2017-09-11 08:37:35 -0600

    Uploaded files
    I hope this works!

---
+++ CMartinezHW1.php
@@ -0,0 +1,32 @@
+<?php
+echo "<h1>"."The ".$_POST["animal0"]."</h1>";
+echo "<h3>"."A variation of "."<i>"."The Tyger"."</i>"." by William Blake"."</h3>";
+echo "<b>".$_POST["animal0"]."</b>"."! "."<b>".$_POST["animal0"]."</b>". "! Burning bright"."<br>".
+     "In the forests of the night,"."<br>".
+     "What "."<b>".$_POST["adjective0"]."</b>"." "."<b>".$_POST["bodypart0"]."</b>"." or eye"."<br>".
+     "Could frame thy "."<b>".$_POST["adjective1"]."</b>"." symmetry?"."<br>".
+     "<br>".
+     "In what distant deeps or skies"."<br>".
+     "Burnt the "."<b>".$_POST["noun0"]."</b>"." of thine eyes?"."<br>".
+     "On what "."<b>".$_POST["bodypart1"]."</b>"." dare he aspire?"."<br>".
+     "What the "."<b>".$_POST["bodypart2"]."</b>"." dare seize the fire?"."<br>".
+     "<br>".
+     "And what "."<b>".$_POST["bodypart3"]."</b>".", & what art,"."<br>".
+     "Could twist the "."<b>".$_POST["nounplural0"]."</b>"." of thy heart?"."<br>".
+     "And when thy heart began to beat,"."<br>".
+     "What dread hand? & what dread feet?"."<br>".
+     "<br>".
+     "What the "."<b>".$_POST["noun1"]."</b>".", what the chain?"."<br>".
+     "In what "."<b>".$_POST["noun2"]."</b>"." was thy brain?"."<br>".
+     "What the "."<b>".$_POST["noun3"]."</b>".", what dread grasp"."<br>".
+     "Dare its "."<b>".$_POST["adjective2"]."</b>"." terrors clasp?"."<br>".
+     "<br>".
+     "When the "."<b>".$_POST["noun4"]."</b>"." threw down their spears,"."<br>".
+     "And water'd "."<b>".$_POST["place0"]."</b>"." with their tears,"."<br>".
+     "Did he smile his work to see?"."<br>".
+     "Did he who made the "."<b>".$_POST["animal1"]."</b>"." make thee?"."<br>".
+     "<br>".
+     "<b>".$_POST["animal0"]."</b>"."! "."<b>".$_POST["animal0"]."</b>"."! Burning bright"."<br>".
+     "In the forests of the night,"."<br>".
+     "What "."<b>".$_POST["adjective0"]."</b>"." "."<b>".$_POST["bodypart0"]."</b>"." or eye"."<br>".
+     "Dare frame thy "."<b>".$_POST["adjective1"]."</b>"." symmetry?";
